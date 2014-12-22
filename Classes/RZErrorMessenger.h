27ce3e0b73650b952a9f46e1901b56e2b9cba4f0
adamhrz <adam.howitt@raizlabs.com>
2014-12-22 10:25:16 -0500

    Added RZErrorMessenger classes

---
+++ Classes/RZErrorMessenger.h
@@ -0,0 +1,172 @@
+//
+//  RZErrorMessenger.h
+//  bhphoto
+//
+//  Created by alex.rouse on 8/11/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+@import Foundation;
+#import "RZMessagingWindow.h"
+
+/**
+ *  The color possibilities for the Error messages
+ */
+typedef NS_ENUM(u_int8_t, BHErrorMessangerColor)
+{
+    kBHErrorMessangerColorRed,
+    kBHErrorMessangerColorBlue
+};
+
+/**
+ *  The Severity of the error message.  Controls the color the message is displayed in.
+ */
+typedef NS_ENUM(u_int8_t, BHErrorMessageSeverity)
+{
+    /**
+     *  A severe error message.  Call when the contents need to be shown as an error
+     */
+    kBHErrorMessageSeveritySevere,
+    /**
+     *  a minor message.  Call when the contents are more of a notificaiton.
+     */
+    kBHErrorMessageSeverityMinor
+};
+
+@interface RZErrorMessenger : NSObject
+
+/**
+ *  Displays an error given a title and detail.  Defaults to A weak error and Severe alert type.
+ *  Also sets animated to YES.
+ *
+ *  @param title  the title of the error.
+ *  @param detail the detail message of the error.
+ */
++ (NSError *)displayErrorWithTitle:(NSString *)title detail:(NSString *)detail;
+
+/**
+ *  Displays an error given a title and details.  Defaults to A weak error and Severe alert type.
+ *  Also sets animated to YES.
+ *
+ *  @param title    The title of the error.
+ *  @param details  An array of strings to be concatenated into an error message.
+ */
++ (NSError *)displayErrorWithTitle:(NSString *)title details:(NSArray *)details;
+
+/**
+ *  Displays an error given a title and detail as well as a color.  Default Severity type is Weak.
+ *
+ *  @param title  The title of the error
+ *  @param detail The detail message of the error
+ *  @param color  The color to display the error
+ *
+ *  @return The new error object.
+ */
++ (NSError *)displayErrorWithTitle:(NSString *)title detail:(NSString *)detail color:(BHErrorMessangerColor)color;
+
+/**
+ *  Displays an error.  Defaults to weak and Red.  Also is Animated
+ *
+ *  @param error The error to display.
+ */
++ (NSError *)displayError:(NSError *)error;
+
+
+/**
+ *  Creates and displays error and updates it with a couple standard error cases.  If none of those exist it uses the
+ *  passed in title and detail as its text.
+ *
+ *  @param title  the fallback title
+ *  @param detail the fallback detail
+ *  @param error  the error that will be checked for standard cases.
+ */
++ (NSError *)displayErrorWithTitle:(NSString *)title detail:(NSString *)detail error:(NSError *)error;
+
+/**
+ *  This displays an error with the strength passed in.  This should be used as the primary
+ *  method for displaying any error messages.
+ *
+ *  @param error    The error container the required information
+ *  @param strength The strength of the error message displayed.
+ *  @param animated if the error should be displayed animated.
+ */
++ (NSError *)displayError:(NSError *)error withStrength:(RZMessageStrength)strength animated:(BOOL)animated;
+
+/**
+ *  This displays an error with the strength and color passed in.  All other presentation 
+ *  methods pass through this method.
+ *  Error.localizedDescription will be used for the title of the error alert.
+ *  Error.localizedRecoverSuggestion will be used for the detail text of the error.
+ *
+ *  @param error    The error container information about the message.
+ *  @param strength The strength of the error message displayed.
+ *  @param color    The color of the error message.
+ *  @param animated If the error should animate.
+ *
+ *  @return An error instance that is modified.  Used to dismiss the message by an error.
+ */
++ (NSError *)displayError:(NSError *)error withStrength:(RZMessageStrength)strength color:(BHErrorMessangerColor)color animated:(BOOL)animated;
+
+
+/**
+ *  Hides the displayed error message.
+ *
+ *  @param animated if the hide should be animated
+ */
++ (void)hideErrorAnimated:(BOOL)animated;
+
+/**
+ *  Hides an error based on the error instance.  If the error isn't displayed yet it will be 
+ *  removed from the queue.
+ *
+ *  @param error         The error that is to be removed
+ *  @param animated      if the hide should be animated.
+ */
++ (void)hideError:(NSError *)error animated:(BOOL)animated;
+
+/**
+ *  Hides all the errors that displayed and destroys the queue of animations.
+ */
++ (void)hideAllErrors;
+
+
+/**
+ *  Creates an error and updates it with a couple standard error cases.  If none of those exist it uses the
+ *  passed in title and detail as its text.
+ *
+ *  @param title  the fallback title
+ *  @param detail the fallback detail
+ *  @param error  the error that will be checked for standard cases.
+ */
++ (NSError *)errorWithDisplayTitle:(NSString *)title detail:(NSString *)detail error:(NSError *)error;
+
++ (BOOL)isErrorCurrentlyDisplayed;
+
++ (RZMessageStrength)strengthOfDisplayedError;
+
+@end
+
+/**
+ *  Error extensions to support the method of display from the error messanger.
+ */
+@interface NSError (BHErrorMessanger)
+
+/**
+ *  Updates the color on the userInfo object of the NSError
+ *
+ *  @note  This returns a new instance of NSError since it isn't mutable
+ *
+ *  @param color The color stored in the NSError
+ *
+ *  @return A new NSError instance.
+ */
+- (NSError *)bh_updateColorOnErrorWithColor:(BHErrorMessangerColor)color;
+
+/**
+ *  The color stored in the NSError's userInfo.  This is a convience method.
+ *
+ *  @return The color value.  Default is Red.
+ */
+- (BHErrorMessangerColor)bh_colorFromError;
+
+@end
---
+++ Classes/RZErrorMessenger.m
@@ -0,0 +1,180 @@
+//
+//  RZErrorMessenger.m
+//  bhphoto
+//
+//  Created by alex.rouse on 8/11/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import "RZErrorMessenger.h"
+
+#import "RZMessagingWindow.h"
+
+#import "NSError+BHMutablility.h"
+
+@implementation RZErrorMessenger
+
+#pragma mark - Displaying Errors
++ (NSError *)displayErrorWithTitle:(NSString *)title detail:(NSString *)detail
+{
+    return [self displayErrorWithTitle:title details:[NSArray arrayWithObjects:detail, nil]];
+}
+
++ (NSError *)displayErrorWithTitle:(NSString *)title details:(NSArray *)details
+{
+    NSMutableString *errorString = [NSMutableString string];
+    
+    [details enumerateObjectsUsingBlock:^(NSString *detail, NSUInteger idx, BOOL *stop) {
+        [errorString appendString:detail];
+        
+        if ( idx < [details count] - 1 ) {
+            [errorString appendString:@"\n"];
+        }
+    }];
+    
+    return [self displayErrorWithTitle:title detail:[errorString copy] color:kBHErrorMessangerColorRed];
+}
+
++ (NSError *)displayErrorWithTitle:(NSString *)title detail:(NSString *)detail color:(BHErrorMessangerColor)color
+{
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    
+    if ( title != nil ) {
+        dict[NSLocalizedDescriptionKey] = title;
+    }
+    
+    if ( detail != nil ) {
+        dict[NSLocalizedRecoverySuggestionErrorKey] = detail;
+    }
+    
+    NSError *error = [NSError errorWithDomain:@"com.bhphotovideo.error" code:999 userInfo:dict];
+    
+    return [self displayError:error withStrength:kRZMessageStrengthWeak color:color animated:YES];
+}
+
++ (NSError *)displayErrorWithTitle:(NSString *)title detail:(NSString *)detail error:(NSError *)error
+{
+    NSError *newError = [self errorWithDisplayTitle:title detail:detail error:error];
+    return [self displayError:newError];
+}
+
++ (NSError *)displayError:(NSError *)error
+{
+    return [self displayError:error withStrength:kRZMessageStrengthWeak animated:YES];
+}
+
++ (NSError *)displayError:(NSError *)error withStrength:(RZMessageStrength)strength animated:(BOOL)animated
+{
+    return [self displayError:error withStrength:strength color:kBHErrorMessangerColorRed animated:animated];
+}
+
++ (NSError *)displayError:(NSError *)error withStrength:(RZMessageStrength)strength color:(BHErrorMessangerColor)color animated:(BOOL)animated
+{
+    error = [self performErrorValidationOnError:error];
+    error = [error bh_updateColorOnErrorWithColor:color];
+    [[[BHAppDelegate appDelegate] errorWindow] showMessageFromError:error strength:strength animated:animated];
+    return error;
+}
+
+#pragma mark - Hiding errors
+
++ (void)hideErrorAnimated:(BOOL)animated
+{
+    [self hideError:nil animated:animated];
+}
+
++ (void)hideError:(NSError *)error animated:(BOOL)animated
+{
+    [[[BHAppDelegate appDelegate] errorWindow] hideMessage:error animated:animated];
+}
+
++ (void)hideAllErrors
+{
+    [[[BHAppDelegate appDelegate] errorWindow] hideAllMessagesAnimated:YES];
+}
+
+#pragma mark - Helpers
+
++ (NSError *)errorWithDisplayTitle:(NSString *)title detail:(NSString *)detail error:(NSError *)error
+{
+    if ( error != nil ){
+        
+        if ( [error.domain isEqualToString:kBHWebserviceErrorDomain] ) {
+            if ( [[error.userInfo valueForKey:kBHWebserviceResponseErrorHasErrorTitle] boolValue] ) {
+                NSString *errorTitle = [error localizedDescription];
+                if (errorTitle != nil){
+                    title = errorTitle;
+                }
+            }
+            if ( [[error.userInfo valueForKey:kBHWebserviceResponseErrorHasErrorMessage] boolValue] ) {
+                NSString *errorMessage = [error localizedRecoverySuggestion];
+                if ( errorMessage != nil ){
+                    detail = errorMessage;
+                }
+            }
+        }
+        else {
+            switch ( error.code ) {
+                case NSURLErrorNotConnectedToInternet:
+                    detail = kBHDefaultErrorMessageNoNetwork;
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+    }
+    else {
+        error = [NSError errorWithDomain:@"com.bhphotovideo.error" code:999 userInfo:nil];
+    }
+
+    error = [error bh_updateLocalizedRecoverySuggestion:detail];
+    error = [error bh_updateLocalizedDescription:title];
+    return error;
+}
+
++ (BOOL)isErrorCurrentlyDisplayed
+{
+    return  [[[BHAppDelegate appDelegate] errorWindow] isCurrentlyDisplayingAnError];
+}
+
++ (RZMessageStrength)strengthOfDisplayedError
+{
+    return [[[BHAppDelegate appDelegate] errorWindow] strengthOfDisplayedError];
+}
+
+
+#pragma mark - Private Methods
+
++ (NSError *)performErrorValidationOnError:(NSError *)error
+{
+    if (error.code == NSURLErrorNotConnectedToInternet) {
+        error = [error bh_updateLocalizedRecoverySuggestion:kBHDefaultErrorMessageNoNetwork];
+    }
+    return error;
+}
+
+@end
+
+static NSString * const kBHErrorMessangerErrorKeyColor = @"BHErrorMessangerErrorKeyColor";
+
+@implementation NSError (BHErrorMessanger)
+
+- (NSError *)bh_updateColorOnErrorWithColor:(BHErrorMessangerColor)color
+{
+    NSMutableDictionary *userInfo = [self.userInfo mutableCopy];
+    userInfo[kBHErrorMessangerErrorKeyColor] = @(color);
+    return [NSError errorWithDomain:self.domain code:self.code userInfo:userInfo];
+}
+
+- (BHErrorMessangerColor)bh_colorFromError
+{
+    BHErrorMessangerColor color = kBHErrorMessangerColorRed;
+    NSNumber *errorValue = self.userInfo[kBHErrorMessangerErrorKeyColor];
+    if (errorValue != nil) {
+        color = [errorValue integerValue];
+    }
+    return color;
+}
+
+@end
